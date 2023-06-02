8f01e8a7a6b31ad0d0c8d41933b70aee899abfa6
vitorluizms <vitor.luiz.eer@gmail.com>
2023-06-02 02:16:19 -0300

    Correção no navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -29,8 +29,9 @@ const Navbar = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 
   div {
-    width: 80%;
+    width: 100%;
     height: 70px;
+    margin: 0 3% 0 3%;
 
     display: flex;
     justify-content: space-between;
